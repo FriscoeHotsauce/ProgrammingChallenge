7cac957c9a5b02f4c70e425f6cf52dc87fbd4874
FriscoeHotsauce <iaharris@iastate.edu>
2017-11-16 18:30:29 -0600

    - Initial commit - General structure, dependencies, maven, gitignore

---
+++ prog/src/main/java/client/OpenWeatherClient.java
@@ -0,0 +1,24 @@
+package client;
+
+import dto.WeatherDto;
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
+
+@Headers({
+    "Content-Type: application/json"
+})
+public interface OpenWeatherClient {
+
+    /**
+     *  Make a GET call to openweather and return the results
+     *
+     * @param location the location of the user
+     * @param apiKey provided by program properties
+     * @return a WeatherDto object, parsed by jackson
+     */
+    @RequestLine("GET api.openweathermap.org/data/2.5/weather?q={location}&APPID={api_key}")
+    WeatherDto getWeatherAtLocation(@Param("location") String location, @Param("api_key") String apiKey);
+
+}
---
+++ prog/src/main/java/main/Application.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args){
+
+
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Where are you from?: ");
+
+
+        reader.close();
+
+    }
+}
